a51ab8667d040bf9bbca75e85b8e31156af419a8
Prebre <idabrekk@gmail.com>
2022-05-14 11:46:17 +0200

    Added html, css and javascript
    Added div to html to serve as a container for html added through javascript.
    
    Added javascript to create html from loop.
    
    Added css to style the new elements generated from javascript.
    
    Tidied up the codes. Added "Loading..." text to page.
    
    Added a folder named CSS, moved "styles.css" file to this folder.
    
    Linked css file to html file.

--- Questions/question-2.js
+++ Questions/question-2.js
@@ -1,72 +1,50 @@
 // Question 2
 // Make a call to the Rawg API.
-
-// Go to https://rawg.io/apidocs.
-
-// You'll be given an API Key you can add as a "key" parameter in your fetch request.
-
 // KEY: 7e8ddb30448b419b91a00a3c34ff18ce
-
 // Make a call to the following API endpoint.
+
+// Loop through the results and display the following properties for the first eight results in HTML:
+// name
+// rating
+// number of tags (not the tag details, just the amount of tags)
+
+// Loading indicator should be displayed while the API call is in progress.
+// Be sure to handle any potential errors in the code.
+// Be sure to arrange all file types appropriately.
 
 const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=7e8ddb30448b419b91a00a3c34ff18ce";
 
 const fixResults = document.querySelector(".result");
 
 async function callApi() {
+
         try {
-            const response = await fetch(url);
 
-            const results = await response.json();
+                const response = await fetch(url);
+                const data = await response.json();
+                const results = data.results;
+                const gamesContainer = document.querySelector("#gamelist");
+                gamesContainer.innerHTML = ""
 
-            for(let i = 0; i < 8; i++) {
+                for(let i = 0; i < 8; i++) {
 
-                console.log(results[i]);
+                        console.log(results[i].name);
+                        console.log(results[i].rating);
+                        console.log(results[i].tags.length);
 
-                // console.log(results[i].name);
-                // console.log(results[i].rating);
-                // console.log(results[i].tags.length);
+                        gamesContainer.innerHTML += `<div class="game"><h2>${results[i].name}</h2><p class="rating"><strong>Rating:</strong> ${results[i].rating}</p><p class="tags"><strong>Number of tags:</strong> ${results[i].tags.length}</p></div>`
 
-                // let objectList = document.createElement("div");
-                // let objectName = document.createElement("h3");
-                // let objectRatings = document.createElement("p");
-                // let objectTags = document.createElement("p");
-
-                // objectName.innerHTML = results[i].name;
-                // objectRatings.innerHTML = results[i].rating
-                // objectTags.innerHTML = results[i].tags.length;
-
-                // objectList.append(objectName, objectRatings, objectTags);
-
-                if(i === 7) {
-                    break;
+                        if(i === 7) {
+                                break;
+                        }
                 }
-            }
-
-            console.log(results);
-
+                
         } catch (err) {
 
             console.log(err);
-            fixResults.innerHTML = e.toString("Error: No value found");
-        
+
+            fixResults.innerHTML = err.toString("Error: No value found");
         }
 }
 
 callApi();
-
-
-// Loop through the results and display the following properties in HTML, but only for the first eight results:
-// name
-// rating
-// number of tags (not the tag details, just the amount of tags)
-// The styling for this assignment is not important but loading indicator should be displayed while the API call is in progress.
-
-
-// Be sure to handle any potential errors in the code.
-
-// You can use either regular promise or async/await syntax to make the call.
-
-// Be sure to arrange all file types appropriately, consult the repos from the lessons for examples.
-
-
